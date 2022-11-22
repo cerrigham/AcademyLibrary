0bbea09bde5c2884ec0162ca0585baceb67e7c7b
cerrigham <cerringham@gmail.com>
2022-11-22 11:59:22 +0100

    fix map data into AcademyLibraryMain while creating a library

--- src/main/java/it/proactivity/main/AcademyLibraryMain.java
+++ src/main/java/it/proactivity/main/AcademyLibraryMain.java
@@ -8,15 +8,16 @@ import java.util.Map;
 public class AcademyLibraryMain {
 
     public static void main(String[] args) {
-        Map<String, String> openingDays = new HashMap<>();
-        openingDays.put("Monday 9.00-12.00","14.00-19.00");
-        openingDays.put("Thuesday 9.00-12.00","14.00-19.00");
-        openingDays.put("Wensday 9.00-12.00","14.00-19.00");
-        openingDays.put("Thursday 9.00-12.00","14.00-19.00");
-        openingDays.put("Friday 9.00-12.00","14.00-19.00");
 
-        LibraryDetail libraryDetail = new LibraryDetail("Biblioteca centrale","Via del centro",
-                false,openingDays);
+        Map<String, String> openingDays = new HashMap<>();
+        openingDays.put("Monday", "9.00-12.00;14.00-19.00");
+        openingDays.put("Tuesday", "9.00-12.00;14.00-19.00");
+        openingDays.put("Wednesday", "9.00-12.00;14.00-19.00");
+        openingDays.put("Thursday", "9.00-12.00;14.00-19.00");
+        openingDays.put("Friday", "9.00-12.00;14.00-19.00");
+
+        LibraryDetail libraryDetail = new LibraryDetail("Biblioteca centrale", "Via del centro",
+                false, openingDays);
 
         System.out.println(libraryDetail);
     }
