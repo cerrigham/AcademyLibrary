b4ee1c31fc90afa4b2c50e990004109610ed6a7b
martina93brusa <martina.brusa@proactivity.it>
2022-11-22 11:55:58 +0100

    Creating constructor of the book Game of Thrones The Winds of Winter

--- src/main/java/it/proactivity/main/AcademyLibraryMain.java
+++ src/main/java/it/proactivity/main/AcademyLibraryMain.java
@@ -1,8 +1,19 @@
 package it.proactivity.main;
 
+import it.proactivity.model.Book;
+import it.proactivity.model.Genre;
+
+import java.util.Date;
+
 public class AcademyLibraryMain {
 
     public static void main(String[] args) {
+        Book book = new Book("Game of Thrones The Winds of Winter", "George R.R. Martin",
+                "Bantam", 1l, 21.25f, 1200,
+                Boolean.FALSE, null, "first reprint", Genre.Fantasy,
+                "99999999999999");
+        System.out.println(book);
         System.out.println("Welcome to Academy Library Project");
+
     }
 }
