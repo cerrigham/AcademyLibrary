9211914436050b392b3f148ec0a90ba557d3a51d
cassarino95 <alessio.cassarino@proactivity.it>
2022-11-22 12:11:41 +0100

    Modified name of the client object

--- src/main/java/it/proactivity/main/AcademyLibraryMain.java
+++ src/main/java/it/proactivity/main/AcademyLibraryMain.java
@@ -9,9 +9,10 @@ public class AcademyLibraryMain {
 
     public static void main(String[] args) {
         System.out.println("Welcome to Academy Library Project");
-        Client client1 = new Client(001l, "Marco", "Rossi", LocalDate.of(2019, Month.AUGUST, 22), "corso regina 4",
+        Client marcoRossi = new Client(001l, "Marco", "Rossi",
+                LocalDate.of(2019, Month.AUGUST, 22), "corso regina 4",
                 Boolean.FALSE, Boolean.TRUE, "marco.rossi22@gmail.com", "3338657482");
-        System.out.println(client1);
+        System.out.println(marcoRossi);
     }
 
 
