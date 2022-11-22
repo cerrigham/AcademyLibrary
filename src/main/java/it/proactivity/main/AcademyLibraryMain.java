6f0ad0e1724e318751a79e776bf72501d725ed79
cerrigham <cerringham@gmail.com>
2022-11-22 09:14:57 +0100

    first dev commit

--- src/main/java/it/proactivity/main/AcademyLibraryMain.java
+++ src/main/java/it/proactivity/main/AcademyLibraryMain.java
@@ -3,6 +3,6 @@ package it.proactivity.main;
 public class AcademyLibraryMain {
 
     public static void main(String[] args) {
-        System.out.println("Academy Library Project");
+        System.out.println("Welcome to Academy Library Project");
     }
 }
