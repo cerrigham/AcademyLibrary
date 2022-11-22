56826295cfa69efb3ebca6f05e7c796490097617
cassarino95 <alessio.cassarino@proactivity.it>
2022-11-22 11:45:13 +0100

    Create new LibraryDetail object in AcademyLibraryMain and test toString method for this object

--- src/main/java/it/proactivity/main/AcademyLibraryMain.java
+++ src/main/java/it/proactivity/main/AcademyLibraryMain.java
@@ -1,8 +1,23 @@
 package it.proactivity.main;
 
+import it.proactivity.model.LibraryDetail;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class AcademyLibraryMain {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Academy Library Project");
+        Map<String, String> openingDays = new HashMap<>();
+        openingDays.put("Monday 9.00-12.00","14.00-19.00");
+        openingDays.put("Thuesday 9.00-12.00","14.00-19.00");
+        openingDays.put("Wensday 9.00-12.00","14.00-19.00");
+        openingDays.put("Thursday 9.00-12.00","14.00-19.00");
+        openingDays.put("Friday 9.00-12.00","14.00-19.00");
+
+        LibraryDetail libraryDetail = new LibraryDetail("Biblioteca centrale","Via del centro",
+                false,openingDays);
+
+        System.out.println(libraryDetail);
     }
 }
