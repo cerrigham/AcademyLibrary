b3eb64d4929d6a70b9f1ddbb5abc3515b04ab389
cerrigham <cerringham@gmail.com>
2022-11-24 14:08:54 +0100

    added junit dependecies into pom.xml

---
+++ src/test/java/TestAddress.java
@@ -0,0 +1,19 @@
+import it.proactivity.model.Address;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+// test at least one positive scenario
+// test all negative scenario
+// test all exception provided for a function
+public class TestAddress {
+
+    @Test
+    public void createAddressTest() {
+        Address address = Address.createAddress("9", "Località campagne", "Monzuno", "Bologna", "Italy");
+
+        assertNotNull(address);
+        assertEquals("9", address.getHouseNumber());
+    }
+}
